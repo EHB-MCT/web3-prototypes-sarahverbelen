1532bcba43325c0ab4c2309c78c8fffc446a1631
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2020-10-28 11:02:02 +0100

    react form

--- ai-api-prototype/ai-frontend/src/App.js
+++ ai-api-prototype/ai-frontend/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+let React = require('react');
+let $ = require('jquery');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      output: {
+        text: '',
+        judgement: null
+      }
+    }
+    this.sendData = this.sendData.bind(this);
+  }
+
+  render() {
+    return ( 
+    <React.Fragment>
+      <header className = "navbar navbar-dark bg-dark" >
+        <h1> AI API Prototype </h1> 
+      </header> 
+      <main className = "container" >
+        <Input sendData = { this.sendData } /> 
+        <Output output = { this.state.output }/> 
+      </main> 
+    </React.Fragment>
+    );
+  }
+
+  sendData(e) {
+    e.preventDefault();
+    let data = $('#input').val();
+    this.setState({
+      output: {
+        text: data,
+        judgement: 0
+      }
+    })
+    console.log(data);
+  }
+}
+
+function Input(props) {
+  return ( 
+  <form className = 'form-group' >
+    <input type = "text" id = "input" className = 'form-control' />  
+    <button id = 'send' onClick = { props.sendData } className = 'btn btn-success' > Send </button> 
+  </form>
   );
+}
+
+
+
+function Output(props) {
+  if (props.output.text !== '') {
+    let klasse;
+    if (props.output.judgement > 0) {
+      klasse = 'alert alert-success';
+    } else if (props.output.judgement === 0) {
+      klasse = 'alert  alert-secondary';
+    } else {
+      klasse = 'alert alert-danger';
+    }
+    return ( 
+    <p className = { klasse }> { props.output.text } </p>
+    );
+  }
+
+  return '';
+
+
 }
 
 export default App;
