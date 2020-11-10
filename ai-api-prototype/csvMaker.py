698337fba2e929ec23151f7f5db199b2316fcc7f
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2020-11-10 13:32:29 +0100

    dutch dataset converting to csv

---
+++ ai-api-prototype/csvMaker.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import csv
+
+entriesNeg = Path('datasets/dutch_reviews/train/neg')
+entriesPos = Path('datasets/dutch_reviews/train/pos')
+
+
+with open('datasets/dutchData.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["text", "sentiment"])
+    for entry in entriesNeg.iterdir():
+        text = entry.read_text(encoding='utf8')
+        writer.writerow([text.replace('\n', '').encode('utf8'), "neg"])
+    for entry in entriesPos.iterdir():
+        text = entry.read_text(encoding='utf8')
+        writer.writerow([text.replace('\n', '').encode('utf8'), "pos"])
